51875d3e4b216ca99eb429e85598b2f5d8d69608
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2025-01-22 08:29:33 +0500

    build log resolved

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,19 +1,19 @@
-"use client"
-import { useState } from "react";
-import Header from "@/app/Components/header";
-import CategoryListing from "@/app/Components/AllProducts";
+// "use client"
+// import { useState } from "react";
+// import Header from "@/app/Components/header";
+// import CategoryListing from "@/app/Components/AllProducts";
 
-export default function ProductPage({ params }: { params: { Category: string } }) {
-  const [searchQuery, setSearchQuery] = useState("");
+// export default function ProductPage({ params }: { params: { Category: string } }) {
+//   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = (query: string) => {
-    setSearchQuery(query);
-  };
+//   const handleSearch = (query: string) => {
+//     setSearchQuery(query);
+//   };
 
-  return (
-    <div>
-      <Header onSearch={handleSearch} />
-      <CategoryListing params={params} searchQuery={searchQuery} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Header onSearch={handleSearch} />
+//       <CategoryListing params={params} searchQuery={searchQuery} />
+//     </div>
+//   );
+// }
